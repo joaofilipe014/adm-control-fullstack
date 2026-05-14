268efe50aadd15e80ab111436f8cc25addb87ee7
JoaoFilipe01 <boss.JoaoFilipe@gmail.com>
2026-05-14 16:20:06 -0300

    First commit

---
+++ adm-control-api/src/main/java/com/admControl/adm_control_api/AdmControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.admControl.adm_control_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmControlApiApplication.class, args);
+	}
+
+}
---
+++ adm-control-api/src/test/java/com/admControl/adm_control_api/AdmControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admControl.adm_control_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
